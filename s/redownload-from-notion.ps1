4e56ca5d598b526c75b4cedcb1007dff522bbf61
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-07-31 12:16:02 -0700

    add s/redownload-from-notion.ps1

---
+++ s/redownload-from-notion.ps1
@@ -0,0 +1,14 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
+
+Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books-2/gen-books.exe
+
+Set-Location -Path cmd/gen-books-2
+go build -o gen-books.exe
+Set-Location -Path ../..
+exitIfFailed
+
+./cmd/gen-books-2/gen-books.exe -no-cache
+Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books-2/gen-books.exe

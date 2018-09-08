c04a1338cba911b9564983e2a267d98f09962ecd
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-09-08 01:30:55 -0700

    extract code highlight code into a separate file

--- cmd/gen-books-2/md_to_html.go
+++ cmd/gen-books-2/md_to_html.go
@@ -4,50 +4,11 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/alecthomas/chroma"
-	"github.com/alecthomas/chroma/formatters/html"
-	"github.com/alecthomas/chroma/lexers"
-	"github.com/alecthomas/chroma/styles"
 	"github.com/gomarkdown/markdown/ast"
-	mdhtml "github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/html"
 )
 
-var (
-	htmlFormatter  *html.Formatter
-	highlightStyle *chroma.Style
-)
-
-func init() {
-	htmlFormatter = html.New(html.WithClasses(), html.TabWidth(2))
-	panicIf(htmlFormatter == nil, "couldn't create html formatter")
-	styleName := "monokailight"
-	highlightStyle = styles.Get(styleName)
-	panicIf(highlightStyle == nil, "didn't find style '%s'", styleName)
-
-}
-
-// based on https://github.com/alecthomas/chroma/blob/master/quick/quick.go
-func htmlHighlight(w io.Writer, source, lang, defaultLang string) error {
-	if lang == "" {
-		lang = defaultLang
-	}
-	l := lexers.Get(lang)
-	if l == nil {
-		l = lexers.Analyse(source)
-	}
-	if l == nil {
-		l = lexers.Fallback
-	}
-	l = chroma.Coalesce(l)
-
-	it, err := l.Tokenise(nil, source)
-	if err != nil {
-		return err
-	}
-	return htmlFormatter.Format(w, highlightStyle, it)
-}
-
-func makeRenderHookCodeBlock(defaultLang string) mdhtml.RenderNodeFunc {
+func makeRenderHookCodeBlock(defaultLang string) html.RenderNodeFunc {
 	return func(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
 		codeBlock, ok := node.(*ast.CodeBlock)
 		if !ok {
@@ -57,7 +18,7 @@ func makeRenderHookCodeBlock(defaultLang string) mdhtml.RenderNodeFunc {
 		if false {
 			fmt.Printf("lang: '%s', code: %s\n", lang, string(codeBlock.Literal[:16]))
 			io.WriteString(w, "\n<pre class=\"chroma\"><code>")
-			mdhtml.EscapeHTML(w, codeBlock.Literal)
+			//html.EscapeHTML(w, codeBlock.Literal)
 			io.WriteString(w, "</code></pre>\n")
 		} else {
 			htmlHighlight(w, string(codeBlock.Literal), lang, defaultLang)

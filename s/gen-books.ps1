92dbeccb2d780a4a9b4c17da838dfe8b823a66d7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-09-23 01:06:27 -0700

    split off Page

--- s/gen-books.ps1
+++ s/gen-books.ps1
@@ -1,14 +1,14 @@
 #!/usr/bin/env pwsh
+
+# you can pass additional args like:
+# -update-go-deps
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
 
-Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books-2/gen-books.exe
-
-Set-Location -Path cmd/gen-books-2
-go build -o gen-books.exe
-Set-Location -Path ../..
+go build -o ./preview.exe github.com/essentialbooks/books/cmd/gen-books-2
 exitIfFailed
 
-./cmd/gen-books-2/gen-books.exe $args
-Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books-2/gen-books.exe
+./preview.exe
+
+Remove-Item -Force -ErrorAction SilentlyContinue ./preview.exe

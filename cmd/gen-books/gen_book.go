5c88f18e210f4885afccb307f1e8fc77cb976cff
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-09-26 19:35:17 -0700

    remove old cmd/gen-book

--- cmd/gen-books/gen_book.go
+++ cmd/gen-books/gen_book.go
@@ -1,144 +1,121 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"bytes"
+	"html/template"
+	"io/ioutil"
+	"log"
 	"path/filepath"
-	"time"
 )
 
-func gen404TopLevel() {
-	d := struct {
-		PageCommon
-		Book *Book
-	}{
-		PageCommon: getPageCommon(),
+const (
+	// top-level directory where .html files are generated
+	destDir = "www"
+	tmplDir = "tmpl"
+)
+
+var ( // directory where generated .html files for books are
+	destEssentialDir = filepath.Join(destDir, "essential")
+	pathAppJS        = "/s/app.js"
+	pathMainCSS      = "/s/main.css"
+	pathFaviconICO   = "/s/favicon.ico"
+)
+
+var (
+	templateNames = []string{
+		"index.tmpl.html",
+		"index-grid.tmpl.html",
+		"book_index.tmpl.html",
+		"chapter.tmpl.html",
+		"article.tmpl.html",
+		"about.tmpl.html",
+		"feedback.tmpl.html",
+		"404.tmpl.html",
 	}
-	path := filepath.Join(destDir, "404.html")
-	execTemplateToFileMaybeMust("404.tmpl.html", d, path)
+	templates = make([]*template.Template, len(templateNames))
+
+	gitHubBaseURL = "https://github.com/essentialbooks/books"
+	siteBaseURL   = "https://www.programming-books.io"
+)
+
+func unloadTemplates() {
+	templates = make([]*template.Template, len(templateNames))
 }
 
-func genIndex(books []*Book) {
-	d := struct {
-		PageCommon
-		Books []*Book
-		//GitHubText string
-		//GitHubURL  string
-	}{
-		PageCommon: getPageCommon(),
-		Books:      books,
-	}
-	path := filepath.Join(destDir, "index.html")
-	execTemplateToFileMaybeMust("index.tmpl.html", d, path)
+func tmplPath(name string) string {
+	return filepath.Join(tmplDir, name)
 }
 
-func genIndexGrid(books []*Book) {
-	d := struct {
-		PageCommon
-		Books []*Book
-	}{
-		PageCommon: getPageCommon(),
-		Books:      books,
+func loadTemplateHelperMaybeMust(name string, ref **template.Template) *template.Template {
+	res := *ref
+	if res != nil {
+		return res
 	}
-	path := filepath.Join(destDir, "index-grid.html")
-	execTemplateToFileMaybeMust("index-grid.tmpl.html", d, path)
-}
-
-func genFeedback() {
-	d := getPageCommon()
-	fmt.Printf("writing feedback.html\n")
-	path := filepath.Join(destDir, "feedback.html")
-	execTemplateToFileMaybeMust("feedback.tmpl.html", d, path)
-}
-
-func genAbout() {
-	d := getPageCommon()
-	fmt.Printf("writing about.html\n")
-	path := filepath.Join(destDir, "about.html")
-	execTemplateToFileMaybeMust("about.tmpl.html", d, path)
-}
-
-func genArticle(article *Article, currChapNo int) {
-	addSitemapURL(article.CanonnicalURL())
-
-	d := struct {
-		PageCommon
-		*Article
-		CurrentChapterNo int
-	}{
-		PageCommon:       getPageCommon(),
-		Article:          article,
-		CurrentChapterNo: currChapNo,
-	}
-
-	path := article.destFilePath()
-	execTemplateToFileSilentMaybeMust("article.tmpl.html", d, path)
-}
-
-func genChapter(chapter *Chapter, currNo int) {
-	addSitemapURL(chapter.CanonnicalURL())
-	for _, article := range chapter.Articles {
-		genArticle(article, currNo)
-	}
-
-	path := chapter.destFilePath()
-	d := struct {
-		PageCommon
-		*Chapter
-		CurrentChapterNo int
-	}{
-		PageCommon:       getPageCommon(),
-		Chapter:          chapter,
-		CurrentChapterNo: currNo,
-	}
-	execTemplateToFileSilentMaybeMust("chapter.tmpl.html", d, path)
-
-	for _, imagePath := range chapter.images {
-		imageName := filepath.Base(imagePath)
-		dst := chapter.destImagePath(imageName)
-		copyFileMaybeMust(dst, imagePath)
-	}
-}
-
-func genBook(book *Book) {
-	fmt.Printf("Started genering book %s\n", book.Title)
-	timeStart := time.Now()
-
-	genBookTOCSearchMust(book)
-
-	// generate index.html for the book
-	err := os.MkdirAll(book.destDir, 0755)
+	path := tmplPath(name)
+	//fmt.Printf("loadTemplateHelperMust: %s\n", path)
+	t, err := template.ParseFiles(path)
 	maybePanicIfErr(err)
 	if err != nil {
+		return nil
+	}
+	*ref = t
+	return t
+}
+
+func loadTemplateMaybeMust(name string) *template.Template {
+	var ref **template.Template
+	for i, tmplName := range templateNames {
+		if tmplName == name {
+			ref = &templates[i]
+			break
+		}
+	}
+	if ref == nil {
+		log.Fatalf("unknown template '%s'\n", name)
+	}
+	return loadTemplateHelperMaybeMust(name, ref)
+}
+
+func execTemplateToFileSilentMaybeMust(name string, data interface{}, path string) {
+	tmpl := loadTemplateMaybeMust(name)
+	if tmpl == nil {
 		return
 	}
+	var buf bytes.Buffer
+	err := tmpl.Execute(&buf, data)
+	maybePanicIfErr(err)
 
-	d := struct {
-		PageCommon
-		Book *Book
-	}{
-		PageCommon: getPageCommon(),
-		Book:       book,
+	d := buf.Bytes()
+	if doMinify {
+		d2, err := minifier.Bytes("text/html", d)
+		maybePanicIfErr(err)
+		if err == nil {
+			totalHTMLBytes += len(d)
+			totalHTMLBytesMinified += len(d2)
+			d = d2
+		}
 	}
+	err = ioutil.WriteFile(path, d, 0644)
+	maybePanicIfErr(err)
+}
 
-	path := filepath.Join(book.destDir, "index.html")
-	execTemplateToFileSilentMaybeMust("book_index.tmpl.html", d, path)
+func execTemplateToFileMaybeMust(name string, data interface{}, path string) {
+	execTemplateToFileSilentMaybeMust(name, data, path)
+}
 
-	path = filepath.Join(book.destDir, "404.html")
-	execTemplateToFileSilentMaybeMust("404.tmpl.html", d, path)
+// PageCommon is a common information for most pages
+type PageCommon struct {
+	Analytics      template.HTML
+	PathAppJS      string
+	PathMainCSS    string
+	PathFaviconICO string
+}
 
-	addSitemapURL(book.CanonnicalURL())
-
-	for i, chapter := range book.Chapters {
-		book.sem <- true
-		book.wg.Add(1)
-		go func(idx int, chap *Chapter) {
-			genChapter(chap, idx)
-			book.wg.Done()
-			<-book.sem
-		}(i+1, chapter)
+func getPageCommon() PageCommon {
+	return PageCommon{
+		Analytics:      googleAnalytics,
+		PathAppJS:      pathAppJS,
+		PathMainCSS:    pathMainCSS,
+		PathFaviconICO: pathFaviconICO,
 	}
-	book.wg.Wait()
-
-	fmt.Printf("Generated %s, %d chapters, %d articles in %s\n", book.Title, len(book.Chapters), book.ArticlesCount(), time.Since(timeStart))
 }

3c221479a0848f141c09f86f71dff15740f470d0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-09-26 19:35:17 -0700

    remove old cmd/gen-book

--- cmd/gen-books/book.go
+++ cmd/gen-books/book.go
@@ -2,78 +2,181 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"path/filepath"
 	"strings"
+	"sync"
 
+	"github.com/essentialbooks/books/pkg/common"
+	"github.com/kjk/notionapi"
 	"github.com/kjk/u"
 )
 
-func updateBookAppJS(book *Book) {
-	srcName := fmt.Sprintf("app-%s.js", book.titleSafe)
-	path := filepath.Join("tmpl", "app.js")
-	d, err := ioutil.ReadFile(path)
-	maybePanicIfErr(err)
+// SoContributor describes a StackOverflow contributor
+type SoContributor struct {
+	ID      int
+	URLPart string
+	Name    string
+}
+
+// Book represents a book
+type Book struct {
+	FileNameBase string // TODO: possibly not needed
+
+	Title     string // "Go", "jQuery" etcc
+	titleSafe string
+	TitleLong string // "Essential Go", "Essential jQuery" etc.
+
+	NotionStartPageID string
+	pageIDToPage      map[string]*notionapi.Page
+	RootPage          *Page
+
+	idToPage map[string]*Page
+
+	fileCache      *FileCache
+	SourceDir      string // dir where source markdown files are
+	destDir        string // dir where destitation html files are
+	SoContributors []SoContributor
+
+	cachedArticlesCount int
+
+	defaultLang string // default programming language for programming examples
+	knownUrls   []string
+
+	// generated toc javascript data
+	tocData []byte
+	// url of combined tocData and app.js
+	AppJSURL string
+
+	// for concurrency
+	sem chan bool
+	wg  sync.WaitGroup
+}
+
+// ContributorCount returns number of contributors
+func (b *Book) ContributorCount() int {
+	return len(b.SoContributors)
+}
+
+// ContributorsURL returns url of the chapter that lists contributors
+func (b *Book) ContributorsURL() string {
+	return b.URL() + "/contributors"
+}
+
+// URL returns url of the book, used in index.tmpl.html
+func (b *Book) URL() string {
+	return fmt.Sprintf("/essential/%s/", b.titleSafe)
+}
+
+// CanonnicalURL returns full url including host
+func (b *Book) CanonnicalURL() string {
+	return urlJoin(siteBaseURL, b.URL())
+}
+
+// ShareOnTwitterText returns text for sharing on twitter
+func (b *Book) ShareOnTwitterText() string {
+	return fmt.Sprintf(`"Essential %s" - a free programming book`, b.Title)
+}
+
+// CoverURL returns url to cover image
+func (b *Book) CoverURL() string {
+	coverName := langToCover[b.titleSafe]
+	return fmt.Sprintf("/covers/%s.png", coverName)
+}
+
+// CoverFullURL returns a URL for the cover including host
+func (b *Book) CoverFullURL() string {
+	return urlJoin(siteBaseURL, b.CoverURL())
+}
+
+// CoverTwitterFullURL returns a URL for the cover including host
+func (b *Book) CoverTwitterFullURL() string {
+	coverName := langToCover[b.titleSafe]
+	coverURL := fmt.Sprintf("/covers/twitter/%s.png", coverName)
+	return urlJoin(siteBaseURL, coverURL)
+}
+
+// ArticlesCount returns total number of articles
+func (b *Book) ArticlesCount() int {
+	if b.cachedArticlesCount != 0 {
+		return b.cachedArticlesCount
+	}
+	panic("NYI")
+	n := 0
+	/*
+		for _, ch := range b.Chapters {
+			n += len(ch.Articles)
+		}
+		// each chapter has 000-index.md which is also an article
+		n += len(b.Chapters)
+	*/
+	b.cachedArticlesCount = n
+	return n
+}
+
+// ChaptersCount returns number of chapters
+func (b *Book) ChaptersCount() int {
+	panic("NYI")
+	return 0
+	// return len(b.Chapters)
+}
+
+// EmbeddedSourceFile represents source file present in the repository
+// and embedded via https://www.onlinetool.io/gitoembed/
+type EmbeddedSourceFile struct {
+	EmbedURL string
+
+	// name of the file
+	FileName string
+	// full path of the file
+	Path string
+
+	FileExists bool
+
+	// content of the file after filtering
+	Lines         []string
+	cachedData    []byte
+	cachedSha1Hex string
+}
+
+// Data returns content of the file
+func (f *EmbeddedSourceFile) Data() []byte {
+	if len(f.cachedData) == 0 {
+		s := strings.Join(f.Lines, "\n")
+		f.cachedData = []byte(s)
+	}
+	return f.cachedData
+}
+
+// RealSha1Hex returns hex version of sha1 of file content
+func (f *EmbeddedSourceFile) RealSha1Hex() string {
+	if f.cachedSha1Hex == "" {
+		f.cachedSha1Hex = u.Sha1HexOfBytes(f.Data())
+	}
+	return f.cachedSha1Hex
+}
+
+func readFilteredSourceFile(path string) ([]string, error) {
+	d, err := common.ReadFileNormalized(path)
 	if err != nil {
-		return
+		return nil, err
 	}
-	if doMinify {
-		d2, err := minifier.Bytes("text/javascript", d)
-		maybePanicIfErr(err)
-		if err == nil {
-			fmt.Printf("Minified %s from %d => %d (saved %d)\n", srcName, len(d), len(d2), len(d)-len(d2))
-			d = d2
-		}
-	}
-
-	d = append(book.tocData, d...)
-	sha1Hex := u.Sha1HexOfBytes(d)
-	name := nameToSha1Name(srcName, sha1Hex)
-	dst := filepath.Join("www", "s", name)
-	err = ioutil.WriteFile(dst, d, 0644)
-	maybePanicIfErr(err)
-	if err != nil {
-		return
-	}
-	book.AppJSURL = "/s/" + name
-	fmt.Printf("Created %s\n", dst)
+	lines := dataToLines(d)
+	lines = removeAnnotationLines(lines)
+	return lines, nil
 }
 
-var didPrint = false
-
-func printKnownURLS(a []string) {
-	if didPrint {
-		return
-	}
-	didPrint = true
-	fmt.Printf("%d known urls\n", len(a))
-	for _, s := range a {
-		fmt.Printf("%s\n", s)
-	}
-}
-
-// turn partial url like "20381" into a full url like "20381-installing"
-func (b *Book) fixupURL(uri string) string {
-	// skip uris that are not article/chapter uris
-	if strings.Contains(uri, "/") {
-		return uri
-	}
-	for _, known := range b.knownUrls {
-		if uri == known {
-			return uri
+// we don't want to show our // :show annotations in snippets
+func removeAnnotationLines(lines []string) []string {
+	var res []string
+	prevWasEmpty := false
+	for _, l := range lines {
+		if strings.Contains(l, "// :show ") {
+			continue
 		}
-		if strings.HasPrefix(known, uri) {
-			//fmt.Printf("fixupURL: %s => %s\n", uri, known)
-			return known
+		if len(l) == 0 && prevWasEmpty {
+			continue
 		}
+		prevWasEmpty = len(l) == 0
+		res = append(res, l)
 	}
-	fmt.Printf("fixupURL: didn't fix up: %s\n", uri)
-	//printKnownURLS(knownURLS)
-	return uri
-}
-
-func (b *Book) makeFixupURL() func(uri string) string {
-	return func(uri string) string {
-		return b.fixupURL(uri)
-	}
+	return res
 }
--- cmd/gen-books/main.go
+++ cmd/gen-books/main.go
@@ -3,383 +3,132 @@ package main
 import (
 	"flag"
 	"fmt"
-	"html/template"
-	"io/ioutil"
-	"os"
 	"path/filepath"
-	"runtime"
-	"sort"
-	"strconv"
-	"strings"
-	"time"
+
+	"html/template"
 
 	"github.com/essentialbooks/books/pkg/common"
-	"github.com/kjk/u"
+	"github.com/kjk/notionapi"
 	"github.com/tdewolff/minify"
-	"github.com/tdewolff/minify/css"
-	"github.com/tdewolff/minify/html"
-	"github.com/tdewolff/minify/js"
 )
 
 var (
-	flgPreview            bool
-	flgUpdateGoPlayground bool
-	flgUpdateOutput       bool
-	flgRecreateOutput     bool
-	flgForce              bool
-	flgUpdateGoDeps       bool
-	flgGenID              bool
+	flgAnalytics string
+	flgNoCache   bool
 
+	soUserIDToNameMap map[int]string
+	googleAnalytics   template.HTML
+	doMinify          bool
+	minifier          *minify.M
+)
+
+var (
+	books = []*Book{
+		&Book{
+			Title:     "Go",
+			TitleLong: "Essential Go",
+			SourceDir: filepath.Join("books", "go"),
+			// "https://www.notion.so/kjkpublic/Essential-Go-2cab1ed2b7a44584b56b0d3ca9b80185"
+			NotionStartPageID: "2cab1ed2b7a44584b56b0d3ca9b80185",
+		},
+	}
+)
+
+const (
+	// https://www.netlify.com/docs/headers-and-basic-auth/#custom-headers
+	netlifyHeaders = `
+# long-lived caching
+/s/*
+  Cache-Control: max-age=31536000
+/*
+  X-Content-Type-Options: nosniff
+  X-Frame-Options: DENY
+  X-XSS-Protection: 1; mode=block
+`
+)
+
+const (
+	googleAnalyticsTmpl = `<script async src="https://www.googletagmanager.com/gtag/js?id=%s"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '%s')
+    </script>
+`
 )
 
 func parseFlags() {
-	flag.BoolVar(&flgPreview, "preview", false, "if true will start watching for file changes and re-build everything")
-	flag.BoolVar(&flgUpdateGoPlayground, "update-go-playground", false, "if true will upgrade links to go playground")
-	flag.BoolVar(&flgUpdateOutput, "update-output", false, "if true, will update ouput files in cached_output")
-	flag.BoolVar(&flgRecreateOutput, "recreate-output", false, "if true, recreates ouput files in cached_output")
-	flag.BoolVar(&flgUpdateGoDeps, "update-go-deps", false, "if true, updates go libraries references in go snippets")
-	flag.BoolVar(&flgGenID, "gen-id", false, "if true, generate unique id")
+	flag.StringVar(&flgAnalytics, "analytics", "", "google analytics code")
+
+	flag.BoolVar(&flgNoCache, "no-cache", false, "if true, disables cache for notion")
 	flag.Parse()
-
-	if flgAnalytics != "" {
-		s := fmt.Sprintf(googleAnalyticsTmpl, flgAnalytics, flgAnalytics)
-		googleAnalytics = template.HTML(s)
-	}
 }
 
-func dirFromBook(book *common.Book) string {
-	return common.MakeURLSafe(book.NewName())
+func downloadBook(book *Book) {
+	notionStartPageID := book.NotionStartPageID
+	book.pageIDToPage = map[string]*notionapi.Page{}
+	loadNotionPages(notionStartPageID, book.pageIDToPage, !flgNoCache)
+	fmt.Printf("Loaded %d pages for book %s\n", len(book.pageIDToPage), book.Title)
+	bookFromPages(book)
 }
 
-func isBookImported(bookDirs []string, book *common.Book) bool {
-	dir := dirFromBook(book)
-	for _, dir2 := range bookDirs {
-		if dir == dir2 {
-			return true
-		}
-	}
-	return false
-}
-
-func getBooksToImport(bookDirs []string) []*common.Book {
-	var res []*common.Book
-	for _, book := range common.BooksToProcess {
-		if isBookImported(bookDirs, book) {
-			res = append(res, book)
-		}
-	}
-	return res
-}
-
-// TODO: probably more
-func getDefaultLangForBook(bookName string) string {
-	s := strings.ToLower(bookName)
-	switch s {
-	case "go":
-		return "go"
-	case "android":
-		return "java"
-	case "ios":
-		return "ObjectiveC"
-	case "microsoft sql server":
-		return "sql"
-	case "node.js":
-		return "javascript"
-	case "mysql":
-		return "sql"
-	case ".net framework":
-		return "c#"
-	}
-	return s
-}
-
-func getBookDirs() []string {
-	dirs, err := common.GetDirs("books")
-	u.PanicIfErr(err)
-	return dirs
-}
-
-func shouldCopyImage(path string) bool {
-	return !strings.Contains(path, "@2x")
-}
-
-func copyFilesRecur(dstDir, srcDir string, shouldCopyFunc func(path string) bool) {
-	createDirMust(dstDir)
-	fileInfos, err := ioutil.ReadDir(srcDir)
-	u.PanicIfErr(err)
-	for _, fi := range fileInfos {
-		name := fi.Name()
-		if fi.IsDir() {
-			dst := filepath.Join(dstDir, name)
-			src := filepath.Join(srcDir, name)
-			copyFilesRecur(dst, src, shouldCopyFunc)
+func iterPages(book *Book, onPage func(*Page) bool) {
+	processed := map[string]bool{}
+	toProcess := []*Page{book.RootPage}
+	for len(toProcess) > 0 {
+		page := toProcess[0]
+		toProcess = toProcess[1:]
+		id := normalizeID(page.NotionPage.ID)
+		if processed[id] {
 			continue
 		}
-
-		src := filepath.Join(srcDir, name)
-		dst := filepath.Join(dstDir, name)
-		shouldCopy := true
-		if shouldCopyFunc != nil {
-			shouldCopy = shouldCopyFunc(src)
-		}
-		if !shouldCopy {
-			continue
-		}
-		if pathExists(dst) {
-			continue
-		}
-		copyFileMust(dst, src)
-	}
-}
-
-func copyCoversMust() {
-	copyFilesRecur(filepath.Join("www", "covers"), "covers", shouldCopyImage)
-}
-
-func getAlmostMaxProcs() int {
-	// leave some juice for other programs
-	nProcs := runtime.NumCPU() - 2
-	if nProcs < 1 {
-		return 1
-	}
-	return nProcs
-}
-
-// copy from tmpl to www, optimize if possible, add
-// sha1 of the content as part of the name
-func copyToWwwAsSha1MaybeMust(srcName string) {
-	var dstPtr *string
-	minifyType := ""
-	switch srcName {
-	case "main.css":
-		dstPtr = &pathMainCSS
-		minifyType = "text/css"
-	case "app.js":
-		dstPtr = &pathAppJS
-		minifyType = "text/javascript"
-	case "favicon.ico":
-		dstPtr = &pathFaviconICO
-	default:
-		u.PanicIf(true, "unknown srcName '%s'", srcName)
-	}
-	src := filepath.Join("tmpl", srcName)
-	d, err := ioutil.ReadFile(src)
-	u.PanicIfErr(err)
-
-	if doMinify && minifyType != "" {
-		d2, err := minifier.Bytes(minifyType, d)
-		maybePanicIfErr(err)
-		if err == nil {
-			fmt.Printf("Compressed %s from %d => %d (saved %d)\n", srcName, len(d), len(d2), len(d)-len(d2))
-			d = d2
+		processed[id] = true
+		toProcess = append(toProcess, page.Pages...)
+		shouldContinue := onPage(page)
+		if !shouldContinue {
+			return
 		}
 	}
-
-	sha1Hex := u.Sha1HexOfBytes(d)
-	name := nameToSha1Name(srcName, sha1Hex)
-	dst := filepath.Join("www", "s", name)
-	err = ioutil.WriteFile(dst, d, 0644)
-	u.PanicIfErr(err)
-	*dstPtr = filepath.ToSlash(dst[len("www"):])
-	fmt.Printf("Copied %s => %s\n", src, dst)
 }
 
-func genSelectedBooks(bookDirs []string) {
-	fmt.Printf("genSelectedBooks: %+v\n", bookDirs)
-	timeStart := time.Now()
-
-	var books []*Book
-	for _, bookName := range bookDirs {
-		book, err := parseBook(bookName)
-		maybePanicIfErr(err)
-		if err != nil {
-			continue
-		}
-		book.sem = make(chan bool, getAlmostMaxProcs())
-		books = append(books, book)
+func buildIDToPage(book *Book) {
+	book.idToPage = map[string]*Page{}
+	fn := func(page *Page) bool {
+		id := normalizeID(page.NotionPage.ID)
+		book.idToPage[id] = page
+		return true
 	}
-	fmt.Printf("Parsed books in %s\n", time.Since(timeStart))
-
-	copyToWwwAsSha1MaybeMust("main.css")
-	copyToWwwAsSha1MaybeMust("app.js")
-	copyToWwwAsSha1MaybeMust("favicon.ico")
-	genIndex(books)
-	genIndexGrid(books)
-	gen404TopLevel()
-	genAbout()
-	genFeedback()
-
-	for _, book := range books {
-		genBook(book)
-	}
-	fmt.Printf("Used %d procs, finished generating all books in %s\n", getAlmostMaxProcs(), time.Since(timeStart))
+	iterPages(book, fn)
 }
 
-func genAllBooks(udpateOutputCache bool) {
-	timeStart := time.Now()
-	clearSitemapURLS()
-	copyCoversMust()
-
-	nProcs := getAlmostMaxProcs()
-
-	var books []*Book
-	for _, bookName := range allBookDirs {
-		book, err := parseBook(bookName)
-		maybePanicIfErr(err)
-		if err != nil {
-			continue
-		}
-		book.sem = make(chan bool, nProcs)
-		books = append(books, book)
+func bookPagesToHTML(book *Book) {
+	nProcessed := 0
+	fn := func(page *Page) bool {
+		notionToHTML(page, book)
+		nProcessed++
+		return true
 	}
-	fmt.Printf("Parsed books in %s\n", time.Since(timeStart))
-
-	copyToWwwAsSha1MaybeMust("main.css")
-	copyToWwwAsSha1MaybeMust("app.js")
-	copyToWwwAsSha1MaybeMust("favicon.ico")
-	genIndex(books)
-	gen404TopLevel()
-	genIndexGrid(books)
-	genAbout()
-	genFeedback()
-
-	for _, book := range books {
-		genBook(book)
-	}
-	writeSitemap()
-	if udpateOutputCache {
-		saveCachedOutputFiles()
-	}
-	fmt.Printf("Used %d procs, finished generating all books in %s\n", nProcs, time.Since(timeStart))
+	iterPages(book, fn)
+	fmt.Printf("bookPagesToHTML: processed %d pages for book %s\n", nProcessed, book.TitleLong)
 }
 
-func loadSOUserMappingsMust() {
-	path := filepath.Join("stack-overflow-docs-dump", "users.json.gz")
-	err := common.JSONDecodeGzipped(path, &soUserIDToNameMap)
-	u.PanicIfErr(err)
-}
-
-func genNetlifyHeaders() {
-	path := filepath.Join("www", "_headers")
-	err := ioutil.WriteFile(path, []byte(netlifyHeaders), 0644)
-	u.PanicIfErr(err)
-}
-
-func genNetlifyRedirects() {
-	// TODO: should be generated from []*Book list
-	s := `
-/essential/go/* /essential/go/404.html 404
-`
-	path := filepath.Join("www", "_redirects")
-	err := ioutil.WriteFile(path, []byte(s), 0644)
-	u.PanicIfErr(err)
-}
-
-var intIDS = make(map[int]bool)
-
-func rememberID(id string) {
-	intID, err := strconv.Atoi(id)
-	u.PanicIfErr(err, "'%s' id is not an int", id)
-	if intIDS[intID] {
-		fmt.Printf("duplicate id: %d\n", intID)
-		os.Exit(1)
-	}
-	intIDS[intID] = true
-}
-
-func genID() {
-	for _, bookName := range allBookDirs {
-		book, err := parseBook(bookName)
-		u.PanicIfErr(err)
-		for _, chapter := range book.Chapters {
-			if chapter.FileNameBase == "contributors" {
-				continue
-			}
-			rememberID(chapter.ID)
-			for _, article := range chapter.Articles {
-				rememberID(article.ID)
-			}
-		}
-	}
-	var idArr []int
-	for id := range intIDS {
-		idArr = append(idArr, id)
-	}
-	sort.Ints(idArr)
-	n := len(idArr)
-	lastID := idArr[n-1]
-	newID := lastID + 1
-	//fmt.Printf("%v\n", idArr)
-	fmt.Printf("id: %d\n", newID)
+func genBookFiles(book *Book) {
+	buildIDToPage(book)
+	bookPagesToHTML(book)
 }
 
 func main() {
-
 	parseFlags()
 
-	if false {
-		genTwitterImagesAndExit()
-	}
+	//flgNoCache = true
 
-	if false {
-		testGetGoPlaygroundShareIDAndExit()
-	}
+	//maybeRemoveNotionCache()
+	for _, book := range books {
+		book.titleSafe = common.MakeURLSafe(book.Title)
+		book.destDir = filepath.Join(destEssentialDir)
 
-	if flgUpdateGoPlayground {
-		goBookDir := filepath.Join("books", "go")
-		updateGoPlaygroundLinks(goBookDir)
-		os.Exit(0)
-	}
-
-	minifier = minify.New()
-	minifier.AddFunc("text/css", css.Minify)
-	minifier.AddFunc("text/html", html.Minify)
-	minifier.AddFunc("text/javascript", js.Minify)
-	// less aggresive minification because html validators
-	// report this as html errors
-	minifier.Add("text/html", &html.Minifier{
-		KeepDocumentTags: true,
-		KeepEndTags:      true,
-	})
-	doMinify = !flgPreview
-
-	reloadCachedOutputFilesMust()
-
-	booksToImport := getBooksToImport(getBookDirs())
-	for _, bookInfo := range booksToImport {
-		allBookDirs = append(allBookDirs, bookInfo.NewName())
-	}
-	loadSOUserMappingsMust()
-
-	if flgGenID {
-		genID()
-		os.Exit(0)
-	}
-
-	os.RemoveAll("www")
-	createDirMust(filepath.Join("www", "s"))
-	genNetlifyHeaders()
-	genNetlifyRedirects()
-
-	if flgUpdateGoDeps {
-		updateGoDeps()
-	}
-
-	cacheFilesInDir("books")
-
-	if flgUpdateOutput {
-		gitRemoveCachedOutputFiles()
-	}
-
-	clearErrors()
-	genAllBooks(flgUpdateOutput)
-	printAndClearErrors()
-	if flgUpdateOutput {
-		gitAddachedOutputFiles()
-		return
-	}
-
-	if flgPreview {
-		startPreview()
+		downloadBook(book)
+		genBookFiles(book)
 	}
 }

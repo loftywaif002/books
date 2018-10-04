c571f1658161e59710522076611fae872074c871
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-04 01:05:26 -0700

    restore toc on the left

--- cmd/gen-books/gen_book_toc_search.go
+++ cmd/gen-books/gen_book_toc_search.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/kjk/u"
+)
+
 /*
 Generates a javascript file that looks like:
 
@@ -26,74 +33,73 @@ const (
 	itemIdxFirstSynonym = 5
 )
 
+// TODO: make it recursive and with arbitrary nesting
 func genBookTOCSearchMust(book *Book) {
-	/*
-		var toc [][]interface{}
-		for _, chapter := range book.Chapters {
-			title := strings.TrimSpace(chapter.Title)
-			uri := chapter.FileNameBase
-			tocItem := []interface{}{false, uri, -1, -1, title}
-			toc = append(toc, tocItem)
-			chapIdx := len(toc) - 1
-			u.PanicIf(chapIdx < 0)
+	var toc [][]interface{}
+	for _, chapter := range book.Chapters() {
+		title := strings.TrimSpace(chapter.Title)
+		uri := chapter.URLLastPath()
+		tocItem := []interface{}{false, uri, -1, -1, title}
+		toc = append(toc, tocItem)
+		chapIdx := len(toc) - 1
+		u.PanicIf(chapIdx < 0)
 
-			headings := chapter.Headings()
+		headings := chapter.Headings
+		for _, heading := range headings {
+			title := heading.Text
+			id := heading.ID
+			if len(id) > 0 {
+				id = uri + "#" + id
+			}
+			tocItem = []interface{}{false, id, chapIdx, -1, title}
+			toc = append(toc, tocItem)
+		}
+
+		for _, article := range chapter.Pages {
+			title := strings.TrimSpace(article.Title)
+			uri := article.URLLastPath()
+			tocItem = []interface{}{false, uri, chapIdx, -1, title}
+			for _, syn := range article.Search {
+				tocItem = append(tocItem, syn)
+			}
+			toc = append(toc, tocItem)
+
+			headings := article.Headings
+			articleIdx := len(toc) - 1
 			for _, heading := range headings {
 				title := heading.Text
 				id := heading.ID
 				if len(id) > 0 {
 					id = uri + "#" + id
 				}
-				tocItem = []interface{}{false, id, chapIdx, -1, title}
+				tocItem = []interface{}{false, id, articleIdx, -1, title}
 				toc = append(toc, tocItem)
 			}
+		}
+	}
 
-			for _, article := range chapter.Articles {
-				title := strings.TrimSpace(article.Title)
-				uri := article.FileNameBase
-				tocItem = []interface{}{false, uri, chapIdx, -1, title}
-				for _, syn := range article.SearchSynonyms {
-					tocItem = append(tocItem, syn)
-				}
-				toc = append(toc, tocItem)
-
-				headings := article.Headings()
-				articleIdx := len(toc) - 1
-				for _, heading := range headings {
-					title := heading.Text
-					id := heading.ID
-					if len(id) > 0 {
-						id = uri + "#" + id
-					}
-					tocItem = []interface{}{false, id, articleIdx, -1, title}
-					toc = append(toc, tocItem)
-				}
+	// set first child idx from parent idx
+	for i, tocItem := range toc {
+		parentIdx := tocItem[itemIdxParent].(int)
+		if parentIdx != -1 {
+			parentTocItem := toc[parentIdx]
+			idx := parentTocItem[itemIdxFirstChild].(int)
+			if idx == -1 {
+				parentTocItem[itemIdxFirstChild] = i
 			}
 		}
+	}
 
-		// set first child idx from parent idx
-		for i, tocItem := range toc {
-			parentIdx := tocItem[itemIdxParent].(int)
-			if parentIdx != -1 {
-				parentTocItem := toc[parentIdx]
-				idx := parentTocItem[itemIdxFirstChild].(int)
-				if idx == -1 {
-					parentTocItem[itemIdxFirstChild] = i
-				}
-			}
-		}
-
-		var d []byte
-		var err error
-		if doMinify {
-			d, err = json.Marshal(toc)
-		} else {
-			d, err = json.MarshalIndent(toc, "", "  ")
-		}
-		u.PanicIfErr(err)
-		s := "gBookToc = " + string(d) + ";"
-		book.tocData = []byte(s)
-	*/
+	var d []byte
+	var err error
+	if doMinify {
+		d, err = json.Marshal(toc)
+	} else {
+		d, err = json.MarshalIndent(toc, "", "  ")
+	}
+	u.PanicIfErr(err)
+	s := "gBookToc = " + string(d) + ";"
+	book.tocData = []byte(s)
 
 	updateBookAppJS(book)
 }

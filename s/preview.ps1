d2196407985925d1c6c89f3fab8ced1a42fcb83f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-04 23:42:34 -0700

    update output files

--- s/preview.ps1
+++ s/preview.ps1
@@ -11,5 +11,5 @@ Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
 go build -o ./gen.exe ./cmd/gen-books
 exitIfFailed
 
-./gen.exe -preview -analytics UA-113489735-1 $args
+./gen.exe -preview -analytics UA-113489735-1 -update-output $args
 Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe

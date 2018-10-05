274362ecf301e258b951e62f52330911f61986f3
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-04 21:50:28 -0700

    add s/netlify-draft-deploy.ps1

--- s/travis_build.sh
+++ s/travis_build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -u -e -o pipefail -o verbose
 
-cd cmd/gen-books
-go build -o gen-books
-cd ../..
+go build -o gen-books ./cmd/gen-books
 
-./cmd/gen-books/gen-books -analytics UA-113489735-1
+./gen-books -analytics UA-113489735-1
 
 if [ -z ${NETLIFY_TOKEN+x} ]
 then
--- s/travis_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -u -e -o pipefail -o verbose
-
-cd cmd
-go get -v -u ./...
-cd ../pkg
-go get -v -u ./...
-cd ..

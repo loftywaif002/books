8e09036c5ad204c15b27c04c605e4c4a3d960389
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-04 22:33:32 -0700

    update output files

--- books/go/0020-basic-types/zero_values.go
+++ books/go/0020-basic-types/zero_values.go
@@ -4,34 +4,34 @@ import "fmt"
 
 func main() {
 	// :show start
-	var zeroBool bool             // = false
-	var zeroInt int               // = 0
-	var zeroF32 float32           // = 0.0
-	var zeroStr string            // = ""
-	var zeroPtr *int              // = nil
-	var zeroSlice []uint32        // = nil
-	var zeroMap map[string]int    // = nil
-	var zeroInterface interface{} // = nil
-	var zeroChan chan bool        // = nil
-	var zeroArray [5]int          // = [0, 0, 0, 0, 0]
+	var zeroBool bool
+	var zeroInt int
+	var zeroF32 float32
+	var zeroStr string
+	var zeroPtr *int
+	var zeroSlice []uint32
+	var zeroMap map[string]int
+	var zeroInterface interface{}
+	var zeroChan chan bool
+	var zeroArray [5]int
 	type struc struct {
 		a int
 		b string
 	}
-	var zeroStruct struc    // = { a: 0, b: ""}
-	var zeroFunc func(bool) // = nil
+	var zeroStruct struc
+	var zeroFunc func(bool)
 
-	fmt.Printf("zeroBool: %v\n", zeroBool)
-	fmt.Printf("zeroInt: %v\n", zeroInt)
-	fmt.Printf("zeroF32: %v\n", zeroF32)
-	fmt.Printf("zeroStr: %#v\n", zeroStr)
-	fmt.Printf("zeroPtr: %v\n", zeroPtr)
-	fmt.Printf("zeroSlice: %#v\n", zeroSlice)
-	fmt.Printf("zeroMap: %#v\n", zeroMap)
-	fmt.Printf("zeroInterface: %v\n", zeroInterface)
-	fmt.Printf("zeroChan: %v\n", zeroChan)
-	fmt.Printf("zeroArray: %v\n", zeroArray)
-	fmt.Printf("zeroStruct: %#v\n", zeroStruct)
-	fmt.Printf("zeroFunc: %v\n", zeroFunc)
+	fmt.Printf("zero bool:       %v\n", zeroBool)
+	fmt.Printf("zero int:        %v\n", zeroInt)
+	fmt.Printf("zero f32:        %v\n", zeroF32)
+	fmt.Printf("zero string:     %#v\n", zeroStr)
+	fmt.Printf("zero ptr:        %v\n", zeroPtr)
+	fmt.Printf("zero slice:      %#v\n", zeroSlice)
+	fmt.Printf("zero map:        %#v\n", zeroMap)
+	fmt.Printf("zero interface:  %v\n", zeroInterface)
+	fmt.Printf("zero channel:    %v\n", zeroChan)
+	fmt.Printf("zero array:      %v\n", zeroArray)
+	fmt.Printf("zero struct:     %#v\n", zeroStruct)
+	fmt.Printf("zero function:   %v\n", zeroFunc)
 	// :show end
 }

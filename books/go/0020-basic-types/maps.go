b105b1a6c5ab780bc03ecf74a3a55ca3fe987848
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-06 17:41:18 -0700

    tweak maps.go example

--- books/go/0020-basic-types/maps.go
+++ books/go/0020-basic-types/maps.go
@@ -6,15 +6,17 @@ func main() {
 	// :show start
 	m := make(map[string]int)
 	m["number3"] = 3
+
 	k := "number3"
 	if n, ok := m[k]; ok {
-		fmt.Printf("value of %s is %d\n", k, n)
+		fmt.Printf("value for key '%s' is %d\n", k, n)
 	} else {
 		fmt.Printf("key '%s' doesn't exist in map m\n", k)
 	}
+
 	k = "number4"
 	if n, ok := m[k]; ok {
-		fmt.Printf("value of %s is %d\n", k, n)
+		fmt.Printf("value for key '%s' is %d\n", k, n)
 	} else {
 		fmt.Printf("key '%s' doesn't exist in map m\n", k)
 	}

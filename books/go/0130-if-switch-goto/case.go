53f92bcfb5d312152ef7ca6a7e94b9f7ced580ea
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-06 23:05:24 -0700

    tweak case.go

--- books/go/0130-if-switch-goto/case.go
+++ books/go/0130-if-switch-goto/case.go
@@ -6,12 +6,12 @@ func main() {
 	// :show start
 	a := 1
 	switch a {
-	case 1:
-		fmt.Printf("a is 1\n")
+	case 1, 3:
+		fmt.Printf("a is 1 or 3\n")
 	case 2:
 		fmt.Printf("a is 2\n")
 	default:
-		fmt.Printf("a is not 1 or 2 but %d\n", a)
+		fmt.Printf("default: a is %d\n", a)
 	}
 	// :show end
 }

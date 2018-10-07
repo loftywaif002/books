c39b209fd468d0d1195361371e6f1a6ab56e2e35
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-06 17:46:24 -0700

    tweak maps.go example

--- books/go/0020-basic-types/maps.go
+++ books/go/0020-basic-types/maps.go
@@ -7,18 +7,23 @@ func main() {
 	m := make(map[string]int)
 	m["number3"] = 3
 
-	k := "number3"
-	if n, ok := m[k]; ok {
-		fmt.Printf("value for key '%s' is %d\n", k, n)
-	} else {
-		fmt.Printf("key '%s' doesn't exist in map m\n", k)
+	checkKey := func(k string) {
+		if n, ok := m[k]; ok {
+			fmt.Printf("value for key '%s' is %d\n", k, n)
+		} else {
+			fmt.Printf("key '%s' doesn't exist in map\n", k)
+		}
 	}
 
-	k = "number4"
-	if n, ok := m[k]; ok {
-		fmt.Printf("value for key '%s' is %d\n", k, n)
-	} else {
-		fmt.Printf("key '%s' doesn't exist in map m\n", k)
-	}
+	// get value for a key that exists
+	checkKey("number3")
+
+	// get value for a key that doesn't exist
+	checkKey("number4")
+
+	// remove a key
+	delete(m, "number3")
+	fmt.Printf("deleted key 'number3\n")
+	checkKey("number3") // and now it doesn't exist
 	// :show end
 }
